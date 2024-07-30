224fa65df9fcde98bbd1e0943c7ed145c0882a72
jb <jbbbregore099@gmail.com>
2024-07-30 17:27:00 +0800

    test

--- controllers/device.js
+++ controllers/device.js
@@ -5,15 +5,15 @@ const { NETWORK_INTERFACES } = require('../constants/device.constant');
 // Get all valid MAC Addresses for Wi-Fi
 exports.getAllHardwareIds = async (req, res, next) => {
   try {
-    const adapters = await getNetworkAdapters();
-    const macAddresses = adapters
-      .filter((a) => {
-        const adapterName = a.name.toLowerCase();
-        return NETWORK_INTERFACES.some((iface) => adapterName.includes(iface.toLowerCase()));
-      })
-      .map((a) => a.macAddress);
+    // const adapters = await getNetworkAdapters();
+    // const macAddresses = adapters
+    //   .filter((a) => {
+    //     const adapterName = a.name.toLowerCase();
+    //     return NETWORK_INTERFACES.some((iface) => adapterName.includes(iface.toLowerCase()));
+    //   })
+    //   .map((a) => a.macAddress);
 
-    return res.status(200).json({ hardwareIds: macAddresses });
+    return res.status(200).json({ hardwareIds: ['abs'] });
   } catch (err) {
     console.log(err);
     const error = new HttpError(err, req, 'Something went wrong, please try again.', 500);
